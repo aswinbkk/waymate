432ddd5cc132dfe5b01ce896849f4d1348b8a755
aswinbkk <aswinbaburajkk@gmail.com>
2026-04-28 21:53:03 +0530

    ride route update

--- backend/routes/rideRoutes.js
+++ backend/routes/rideRoutes.js
@@ -6,7 +6,7 @@ const AuthMiddleware = require('../middleWare/middleWare')
 router.post('/create' ,AuthMiddleware,rideController.createRide);
 router.get('/allRide', rideController.getAllRides);
 router.put('/update/:id', rideController.updateRide);
-router.delete('delete/:id', rideController.deleteRide);
+router.delete('/delete/:id', rideController.deleteRide);
 // router.put('/updateStatus/:rideId', rideController.updateRideStatus);
 
 module.exports = router;

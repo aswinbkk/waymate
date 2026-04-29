b13d188814da387e7a522d8d74c0a89aa289dda2
aswin <aswinbaburajkk@gmail.com>
2026-04-29 15:14:07 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+ background-color: #6200ea;
+ color: white;
+ padding: 10px 20px;
+ border: none;
+ border-radius: 5px;
+ font-size: 1rem;
+ cursor: pointer;
+ &:hover {
+   background-color: #ff69b4;
+ }
+`;
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
+ return (
+   <div>
+     <h1>Hello Styled Components</h1>
+     <StyledButton>Click Me</StyledButton>
+   </div>
+ );
+};
+export default App;

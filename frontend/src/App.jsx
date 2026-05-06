0fe08adb1b4fefa923d4353ef97547652b48fed5
aswin <aswinbaburajkk@gmail.com>
2026-05-06 14:41:21 +0530

    Navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,10 @@
 import React from "react";
-import styled from "styled-components";
-import NavBar from "../src/components/NavBar"
-
-const StyledButton = styled.button`
- background-color: #6200ea;
- color: white;
- padding: 10px 20px;
- border: none;
- border-radius: 5px;
- font-size: 1rem;
- cursor: pointer;
- &:hover {
-   background-color: #ff69b4;
- }
-`;
+import Navbar from "./components/Navbar"
 
 const App = () => {
  return (
    <div>
-   <NavBar></NavBar>
-     <h1>Hello Styled Components</h1>
-     <StyledButton>Click Me</StyledButton>
+   <Navbar></Navbar>
    </div>
  );
 };
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,68 @@
-import React from 'react'
-import "../style/navbar.css"
+import styled from 'styled-components';
 
-function NavBar() {
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavbarLeft = styled.div`
+  width: 50px;
+`;
+
+const NavbarCenter = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
+
+const NavbarSearch = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavbarCenterUl = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 2em;
+`;
+
+const NavbarRight = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const Button = styled.button`
+  max-width: 70px;
+  color: red;
+  background-color: green;
+`;
+
+const Navbar = () => {
   return (
-    <div>
-        <div class= "nav_container">
-        <div class="logo"><img src="/waymate_standalone_icon.png"></img></div>
-        <div class="nav_center">
-            <ul>
-                <li>Home</li>
-                <li>Search</li>
-                <li>Contact</li>
-                <li>About</li>
-            </ul>
-        </div>
-        <button>Login</button>
-        </div>
-    </div>
-  )
-}
+    <NavbarContainer>
+      <NavbarLeft>
+        <img src="/waymate_standalone_icon.png" alt="logo" />
+      </NavbarLeft>
 
-export default NavBar
+      <NavbarCenter>
+        <NavbarSearch>
+
+        </NavbarSearch>
+        <NavbarCenterUl>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </NavbarCenterUl>
+      </NavbarCenter>
+
+      <NavbarRight>
+        <Button>Login</Button>
+        <Button>Sign Up</Button>
+      </NavbarRight>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
